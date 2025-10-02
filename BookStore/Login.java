3a05c458e5b29a6722c3f3f1c438ad4bf6f680d0
Poohh-494 <sasipong.cha@ku.th>
2025-10-02 19:53:31 +0700

    Add files via upload

--- BookStore/Login.java
+++ BookStore/Login.java
@@ -8,7 +8,7 @@ public class Login extends JFrame implements ActionListener,KeyListener{
     }
     static JFrame up;
     Container cp ;
-    JLabel username , password ;
+    JLabel username , password, us ;
     JTextField t1 ,t2 ;
     JButton b1,b2 ;
     public Login(){
@@ -24,10 +24,11 @@ public class Login extends JFrame implements ActionListener,KeyListener{
     public void setComponent(){
         username = new JLabel("Username : ");
         password = new JLabel("Password : ");
+        us = new JLabel("username must not be special.");
         t1 = new JTextField(20);
         t2 = new JPasswordField(20);
-        b1 = new JButton("Comfirm");
-        b2 = new JButton("Cancel");  
+        b1 = new JButton("Confirm");
+        b2 = new JButton("Undo");  
         t1.setText("");
         t2.setText("");
 
@@ -36,12 +37,25 @@ public class Login extends JFrame implements ActionListener,KeyListener{
         b1.addActionListener(this);
         b2.addActionListener(this);
 
-        username.setBounds(32, 10, 60, 25);
-        password.setBounds(10, 40, 60, 25);
-        t1.setBounds(60, 10, 120, 25);
-        t2.setBounds(60, 40, 180, 25);
-        b1.setBounds(30, 80, 70, 25);
-        b2.setBounds(120, 80, 70, 25);
+        username.setBounds(60, 150, 110, 25);
+        username.setFont(new Font("Helvetica",Font.BOLD,18));
+        us.setBounds(170, 160, 200, 25);
+        us.setFont(new Font("Helvetica",Font.ITALIC,18));
+
+        password.setBounds(60, 220, 110, 25);
+        password.setFont(new Font("Helvetica",Font.BOLD,18));
+
+        t1.setBounds(170, 150, 200, 25);
+        t1.setFont(new Font("Roboto",Font.PLAIN,14));
+
+        t2.setBounds(170, 220, 200, 25);
+        t2.setFont(new Font("Roboto",Font.PLAIN,14));
+
+        b1.setBounds(150, 280, 80, 30);
+        b1.setBackground(new Color(255, 0, 0));
+
+        b2.setBounds(270, 280, 80, 30);
+        b2.setBackground(new Color(255, 0, 0));
         
         cp.add(username); cp.add(t1);
         cp.add(password); cp.add(t2);
@@ -62,25 +76,30 @@ public class Login extends JFrame implements ActionListener,KeyListener{
             String pass = t2.getText();
             Boolean speical = true;
             Boolean userNpass = true;
+            Boolean passchk = true;
             for(Character ch : user.toCharArray()){
                 if(!Character.isAlphabetic(ch)){
                     speical = false;
                 }
             }
-            
+
             if (user.isEmpty() || pass.isEmpty()) {
             userNpass = false;
-
             showMessage("Get Username and Password Please.");
             }
-                
-            if(speical==false){
-                 showMessage("You can't use speical symbol in username.");
+
+            if(pass.length() < 8) {
+                passchk = false;
+                showMessage("Password not correct.");
             }
-            else if(speical == true && userNpass == true) 
+
+            if(speical==false){
+            showMessage("You can't use speical symbol in username.");
+            }
+            else if(speical == true && userNpass == true && passchk == true)
             showMessage("Login successful!");
             
-        }else if(e.getSource() == b2){
+            }else if(e.getSource() == b2){
             t1.setText(""); 
             t2.setText("");
             }
@@ -99,6 +118,4 @@ public class Login extends JFrame implements ActionListener,KeyListener{
     public void keyReleased(KeyEvent e) {
        
     }
-
-
 }
