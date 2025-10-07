745342d6fa025eb0d57499d8635a9276e6a606d8
woramet0 <93065399+woramet0@users.noreply.github.com>
2025-10-07 21:58:17 +0700

    Add files via upload

--- Lib/ShipmentFactory/StandardShipment.java
+++ Lib/ShipmentFactory/StandardShipment.java
@@ -1,5 +1,8 @@
-package Lib.ShipmentFactory;
+package Lib.StrategyPattern;
 
 public class StandardShipment implements Shipment {
-    
+    @Override
+    public String ship() {
+        return "Shipping by Standard";
+    }
 }
