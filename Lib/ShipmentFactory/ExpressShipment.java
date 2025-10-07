fccefa9e99d5fbd8da223e95ad212e18e9afa4aa
woramet0 <93065399+woramet0@users.noreply.github.com>
2025-10-07 21:56:19 +0700

    Add files via upload

--- Lib/ShipmentFactory/ExpressShipment.java
+++ Lib/ShipmentFactory/ExpressShipment.java
@@ -1,5 +1,8 @@
-package Lib.ShipmentFactory;
+package Lib.StrategyPattern;
 
-public class ExpressShipment implements Shipment{
-    
+public class ExpressShipment implements Shipment {
+    @Override
+    public String ship() {
+        return "Shipping by Express";
+    }
 }
