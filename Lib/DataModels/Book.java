2680ad29d4b201df2458921cc9c20ae87444efd5
maaisra <siriwimon.sra@ku.th>
2025-10-08 00:27:46 +0700

    Add files via upload

--- Lib/DataModels/Book.java
+++ Lib/DataModels/Book.java
@@ -1,2 +1,4 @@
-public record Book(String ISBN, String name, String author, Double price) {
+package Lib.DataModels;
+
+public record Book(String ISBN, String name, String author, Double price, int amount) {
 }
--- Lib/DataModels/Order.java
+++ Lib/DataModels/Order.java
@@ -1,3 +1,32 @@
-public class Oder {
+package Lib.DataModels;
+public class Order {
+
+    //Rep
+    private final Book book;
+    private int quantity;
+
+    public Order(Book book, int quantity){
+        this.book = book;
+        this.quantity = quantity;
+        CheckRep();
+    }
+
+    public Book getBook(){
+        return book;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public void increaseQuantity(int amount){
+        this.quantity += amount;
+        CheckRep();
+    }
     
+    private void CheckRep(){
+        if (book == null || quantity < 0) {
+            throw new RuntimeException("Rep invariant violated!");
+        }
+    }
 }
